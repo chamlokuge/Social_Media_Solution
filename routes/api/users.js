bd3b68352d29a69a3add403c21ef186937d26ebc
chamlokuge <chamodilokuge@gmail.com>
2020-10-27 12:16:15 +0530

    Add User Registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator');
+
+//bring in userModel
+const User = require("../../models/User");
 
 // @route GET api/users
 //@desc Test route
@@ -22,12 +27,53 @@ router.post(
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
-    } else{
-        console.log(req.body);
-        res.send('User route');
-        
+    } 
+
+    const { name, email, password } = req.body;
+
+    try{
+
+        //see if user exists
+        let user = await User.findOne({ email });
+
+        if(user) {
+            return res.status(400).json({ errors: [{ msg: 'User already exists '}]});
+        }
+
+        //get users gravatar
+        const avatar = gravatar.url(email, {
+            s: '200',
+            r: 'pg' ,
+            d: 'mm'
+        }) 
+
+        user = new User({
+            name,
+            email,
+            avatar,
+            password
+        });
+
+        //encrypt password
+
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        //return jsonwebtoken
+
+        res.send('User Registered');
+
+    }catch (err){
+        console.error(err);
+        res.status(500).send('Server error');
     }
     
+        
+    
+
 // router.post('/', (req, res) => {
 //     console.log(req.body);
 //     res.send('User route');
