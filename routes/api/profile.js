a37dc71e7122808d9996a9c529aff090c69a1911
chamlokuge <chamodilokuge@gmail.com>
2020-10-28 00:20:01 +0530

    Add delete route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -147,5 +147,31 @@ router.get('/user/:user_id', async (req, res) => {
     }
 });
 
+// @route DELETE api/profile
+//@desc delete profile by user ID
+//@access Private
+
+router.delete('/', auth, async (req, res) => {
+    try {
+        //@todo - remove users posts
+        
+        //remove profile
+        await Profile.findOneAndRemove({ user: req.user.id });
+
+        //remove user
+        await User.findOneAndRemove({ _id: req.user.id });
+
+        res.json({msg: 'User deleted'});
+         
+    } catch (err) {
+        console.error(err.message);
+        if(err.kind == 'ObjectId') {
+            return res.status(400).json({ msg: 'Profile not found'});
+        }
+        res.status(500).send('Server Error');
+    }
+});
+
+
 
 module.exports = router;
