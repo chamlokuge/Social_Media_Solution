c4c62e967a5c159ba6c51d92a7bda0eeca5f30e7
chamlokuge <chamodilokuge@gmail.com>
2020-10-27 12:15:35 +0530

    Add User Registration

--- config/db.js
+++ config/db.js
@@ -6,10 +6,11 @@ const connectDB = async () => {
     try {
         await mongoose.connect(db, {
             useNewUrlParser: true,
-            useUnifiedTopology:true
+            useUnifiedTopology:true,
+            useCreateIndex: true
         });
         console.log("MongoDb Connected ...");
-        
+
     } catch(err) {
         console.error(err.message);
         //exit process with failure
