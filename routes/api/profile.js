d2ce18a1b965b989cd0d73a452ef177617107a0e
chamlokuge <chamodilokuge@gmail.com>
2020-10-28 12:53:53 +0530

    Add delete route to experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -229,4 +229,46 @@ router.put('/experience', [ auth, [
 
 });
 
+// @route DELETE api/profile/experience/:exp_id
+//@desc delete experience from profile
+//@access Private
+
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+
+    try {
+    const foundProfile = await Profile.findOne({ user: req.user.id });
+
+    foundProfile.experience = foundProfile.experience.filter(
+      (exp) => exp._id.toString() !== req.params.exp_id
+    );
+
+    await foundProfile.save();
+    return res.status(200).json(foundProfile);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ msg: 'Server error' });
+  }
+});
+
+//     try {
+//         const profile = await Profile.findOne({ user: req.user.id }); 
+
+//         //get remove index
+//         const removeIndex = profile.experience
+//             .map(item => item.id)
+//             .indexOf(req.params.exp_id);
+
+//         profile.experience.splice(removeIndex, 1);
+
+//         await profile.save();
+
+//         res.json(profile);
+
+
+//     } catch (err) {
+//         console.error(err.message);
+//         res.status(500).send('Server Error');
+//     }
+// })
+
 module.exports = router;
