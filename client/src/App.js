46a6af5f65b2b4078fa6d713500feef63bd27045
chamlokuge <chamodilokuge@gmail.com>
2020-10-29 18:50:28 +0530

    Add react app

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React, { Fragment } from 'react';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+
+const App = () => (
+  
+    <Fragment>
+      <Navbar />
+      <Landing />
+    </Fragment>
+   
+);
+
+export default App;
