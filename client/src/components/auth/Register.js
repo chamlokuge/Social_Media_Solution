e7b41b3e2d0662912af78932a47fcf4babf0e0b8
chamlokuge <chamodilokuge@gmail.com>
2020-10-29 21:40:36 +0530

    Modify register form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,6 @@
 import React , { Fragment, useState } from 'react';
-import axios from 'axios';
+//import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
 
@@ -19,27 +20,28 @@ const Register = () => {
         if(password !== password2){
             console.log('Passwords do not match');
         } else {
-            const newUser = {
-                name,
-                email,
-                password
-            }
+            // const newUser = {
+            //     name,
+            //     email,
+            //     password
+            // }
             
-            try {
-                const config = {
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                }
+            // try {
+            //     const config = {
+            //         headers: {
+            //             'Content-Type': 'application/json'
+            //         }
+            //     }
 
-                const body = JSON.stringify(newUser);
+            //     const body = JSON.stringify(newUser);
 
-                const res = await axios.post('/api/users', body, config);
+            //     const res = await axios.post('/api/users', body, config);
 
-                console.log(res.data);
-            } catch (err) {
-                console.error(err.response.data);
-            }
+            //     console.log(res.data);
+            // } catch (err) {
+            //     console.error(err.response.data);
+            // }
+            console.log('SUCCESS');
         }
     }
 
@@ -79,7 +81,7 @@ const Register = () => {
         <input type="submit" className="btn btn-primary" value="Register" />
       </form>
       <p className="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
+        Already have an account? <Link to="/login">Sign In</Link>
       </p>
         </Fragment>
     )
