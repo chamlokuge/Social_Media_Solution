a4b4fe0e9c33295e57c8f912c7a75b4b8f762f6e
chamlokuge <chamodilokuge@gmail.com>
2020-10-30 23:12:33 +0530

    Add register action

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -3,9 +3,10 @@ import React , { Fragment, useState } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
 import PropTypes from 'prop-types'
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register }) => {
 
     const [formData, setFormData] = useState({
         name: '',
@@ -44,7 +45,8 @@ const Register = ({ setAlert }) => {
             // } catch (err) {
             //     console.error(err.response.data);
             // }
-            console.log('SUCCESS');
+            
+            register({ name, email, password });
         }
     }
 
@@ -92,6 +94,7 @@ const Register = ({ setAlert }) => {
 
 Register.propTypes = {
     setAlert: PropTypes.func.isRequired,
+    register: PropTypes.func.isRequired,
 };
 
-export default connect(null, { setAlert })(Register);
+export default connect(null, { setAlert, register })(Register);
